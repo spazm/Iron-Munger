283b84e94643a4d2018aab54d4f3312c0aaf46c6
Matt S Trout <matthewt@hercule.scsys.co.uk>
2009-05-15 19:11:57 +0100

    add 'url' attribute to Post.pm (tests fail, will fixup)

--- lib/IronMunger/Post.pm
+++ lib/IronMunger/Post.pm
@@ -2,9 +2,12 @@ use MooseX::Declare;
 
 class IronMunger::Post {
 
+  use MooseX::Types::Moose qw(Str);
   use MooseX::Types::DateTimeX qw(DateTime);
 
-  has at => (isa => DateTime, is => 'rw', required => 1);
+  has at => (isa => DateTime, is => 'ro', required => 1);
+
+  has url => (isa => Str, is => 'ro', required => 1);
 }
 
 1;
